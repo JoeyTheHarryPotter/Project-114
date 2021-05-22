a25b9ec871b4947bc943c774e2fb05663ab8c0db
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-05-22 16:11:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(810, 200);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('my_filter_image.png');
+}
